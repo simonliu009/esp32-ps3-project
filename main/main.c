f6cf6417b5cbefb382dd9a1672c2bf5373dfaad5
Simon Liu <sliu009@gmail.com>
2022-09-13 22:19:41 +0800

    Add select/start button event message, rewrite all button  message format

--- main/main.c
+++ main/main.c
@@ -20,75 +20,85 @@
 void controller_event_cb( ps3_t ps3, ps3_event_t event )
 {
     // Event handling here...
+    if ( event.button_down.select )
+        printf("SELECT button is pressed.\r\n");
+    if ( event.button_up.select )
+        printf("SELECT button is released\r\n");
+
+    if ( event.button_down.start )
+        printf("START button is pressed.\r\n");
+    if ( event.button_up.start )
+        printf("START button is released.\r\n");
+
     if ( event.button_down.cross )
-        printf("The user started pressing the X button\r\n");
+        printf("CROSS button is pressed.\r\n");
     if ( event.button_up.cross )
-        printf("The user released the X button\r\n");
+        printf("CROSS button is released.\r\n");
 
 
     if ( event.button_down.square )
-        printf("The user started pressing the square button\r\n");
+        printf("SQUARE button is pressed.\r\n");
     if ( event.button_up.square )
-        printf("The user released the square button\r\n");
+        printf("SQUARE button is released.\r\n");
 
 
     if ( event.button_down.triangle )
-        printf("The user started pressing the triangle button\r\n");
+        printf("TRIANGLE button is pressed.\r\n");
     if ( event.button_up.triangle )
-        printf("The user released the triangle button\r\n");
+        printf("TRIANGLE button is released.\r\n");
 
 
     if ( event.button_down.circle )
-        printf("The user started pressing the circle button\r\n");
+        printf("CIRCLE button is pressed.\r\n");
     if ( event.button_up.circle )
-        printf("The user released the circle button\r\n");
+        printf("CIRCLE button is released.\r\n");
 
 
     if ( event.button_down.up )
-        printf("The user started pressing the up button\r\n");
+        printf("UP button is pressed.\r\n");
     if ( event.button_up.up )
-        printf("The user released the up button\r\n");
+        printf("UP button is released.\r\n");
 
 
     if ( event.button_down.down )
-        printf("The user started pressing the down button\r\n");
+        printf("DOWN button is pressed.\r\n");
     if ( event.button_up.down )
-        printf("The user released the down button\r\n");
+        printf("DOWN button is released.\r\n");
 
 
     if ( event.button_down.left )
-        printf("The user started pressing the left button\r\n");
+        printf("LEFT button is pressed.\r\n");
     if ( event.button_up.left )
-        printf("The user released the left button\r\n");
+        printf("LEFT button is released.\r\n");
 
 
     if ( event.button_down.right )
-        printf("The user started pressing the right button\r\n");
+        printf("RIGHT button is pressed.\r\n");
     if ( event.button_up.right )
-        printf("The user released the right button\r\n");
+        printf("RIGHT button is released.\r\n");
 
 
     if ( event.button_down.l1 )
-        printf("The user started pressing the l1 button\r\n");
+        printf("L1 button is pressed.\r\n");
     if ( event.button_up.l1 )
-        printf("The user released the l1 button\r\n");
+        printf("L1 button is released.\r\n");
 
 
     if ( event.button_down.l2 )
-        printf("The user started pressing the l2 button\r\n");
+        printf("L2 button is pressed.\r\n");
     if ( event.button_up.l2 )
-        printf("The user released the l2 button\r\n");
+        printf("L2 button is released.\r\n");
 
 
     if ( event.button_down.r1 )
-        printf("The user started pressing the r1 button\r\n");
+        printf("R1 button is pressed.\r\n");
     if ( event.button_up.r1 )
-        printf("The user released the r1 button\r\n");
+        printf("R1 button is released.\r\n");
 
     if ( event.button_down.r2 )
-        printf("The user started pressing the r2 button\r\n");
+        printf("R2 button is pressed.\r\n");
     if ( event.button_up.r2 )
-        printf("The user released the r2 button\r\n");
+        printf("R2 button is released.\r\n");
 
     // left stick
     if ( ps3.analog.stick.lx < 0 )
